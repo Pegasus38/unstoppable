0cddc44c17ebfd3caa17929f82f1a7dee67af9fd
wjj <wujj1997@qq.com>
2022-12-03 23:05:30 +0800

    leetcode No.343 剑指offer 14 pass

--- src/Solution/December.java
+++ src/Solution/December.java
@@ -40,4 +40,40 @@ public class December {
         list.sort(Comparator.naturalOrder());
         return list.get(list.size()-2);
     }
+    //leetcode No.343 pass 规律题 但是数据规模太小 很简单的过了 不用取余 如果数据规模大了 就是剑指offer的14 也是用这个方法
+    public static int integerBreak(int n){
+        long res = 1;
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+        int count = n / 3;
+        int mod = n % 3;
+        if(mod == 0){
+            for(int i = 0;i < count;i++){
+                res *= 3;
+                res %= 1000000007;
+            }
+        }
+        else if(mod == 1){
+            for(int i = 0;i < count-1;i++){
+                res *= 3;
+                res %= 1000000007;
+            }
+            res *= 4;
+            res %= 1000000007;
+        }
+        else {
+            for(int i = 0;i < count;i++){
+                res *= 3;
+                res %= 1000000007;
+            }
+            res *= 2;
+            res %= 1000000007;
+        }
+        return (int)res;
+    }
+
 }
