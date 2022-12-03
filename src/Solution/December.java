c32b16759ce9e0b2142985e35031769a58ba68c3
wjj <wujj1997@qq.com>
2022-12-03 13:07:47 +0800

    leetcode No.1796 pass

--- src/Solution/December.java
+++ src/Solution/December.java
@@ -1,5 +1,10 @@
 package Solution;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
 public class December {
     public static void main(String[] args) {
 
@@ -18,5 +23,21 @@ public class December {
             res[i] = count;
         }
         return res;
+    }
+    //12.3
+    //leetcode No.1796 每日一题 pass 原来8月份就做过了.
+    public static int secondHighest(String s){
+        HashSet<Integer> set = new HashSet<>();
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                set.add(c-'0');
+            }
+        }
+        List<Integer> list = new ArrayList<>(set);
+        if(list.size() < 2){
+            return -1;
+        }
+        list.sort(Comparator.naturalOrder());
+        return list.get(list.size()-2);
     }
 }
