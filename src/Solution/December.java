9dfd2ee52e2ac9c46a49b7656a6248b151d42cd4
wjj <wujj1997@qq.com>
2022-12-04 22:52:38 +0800

    leetcode No.1492 pass

--- src/Solution/December.java
+++ src/Solution/December.java
@@ -75,5 +75,18 @@ public class December {
         }
         return (int)res;
     }
-
+    //12.4
+    //leetcode No.1492 pass
+    public static int kthFactor(int n, int k){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1;i <= n;i++){
+            if(n % i == 0){
+                list.add(i);
+            }
+        }
+        if(k > list.size()){
+            return -1;
+        }
+        return list.get(k-1);
+    }
 }
