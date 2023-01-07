714fc380f5bb39ac3d62d1dc4f71f2ebde9110d4
wjj <wujj1997@qq.com>
2023-01-07 15:50:12 +0800

    leetcode No.1658 pass 新建了个文件 存放今年的所有刷题解答过程

---
+++ src/Solution/LeetCodeAnswer2023.java
@@ -0,0 +1,30 @@
+package Solution;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class LeetCodeAnswer2023 {
+    //今年所有的刷题记录都整合在这里吧 2023加油
+    public static void main(String[] args) {
+    }
+    //1.7
+    //leetcode No.1658 错了两次 第一次没有判断整个和刚好等于x的情况 第二次好像是把sum改成了long 反而不过 真几把服了
+    public static int minOperations(int[] nums, int x){
+        int res = -1;
+        int allSum = Arrays.stream(nums).sum();
+        int sum = 0,target = allSum - x;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        for(int i = 0;i < nums.length;i++){
+            sum += nums[i];
+            if(map.containsKey(sum - target)){
+                res = Math.max(res,i - map.get(sum-target));
+            }
+            map.put(sum,i);
+        }
+        if(map.containsKey(x)){
+            res = Math.max(res,nums.length-1-map.get(x));
+        }
+        return res == -1 ? -1 : nums.length - res;
+    }
+}
