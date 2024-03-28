a493f1d18990eb5989daa4e4ad4b345e56db1c7c
Rek <wujj1997@gmail.com>
2024-03-29 00:53:14 +0800

    leetcode No.2908 pass

--- src/Solution/LeetCodeAnswer.java
+++ src/Solution/LeetCodeAnswer.java
@@ -1694,4 +1694,19 @@ public class LeetCodeAnswer {
         }
         return set.size();
     }
+    //24.3.29
+    //leetcode No.2908 easy 三层循环暴力
+    public static int minimumSum(int[] nums) {
+        int res = Integer.MAX_VALUE;
+        for(int i = 0;i < nums.length - 2;i++){
+            for(int j = i + 1;j < nums.length - 1;j++){
+                for(int k = j + 1;k < nums.length;k++){
+                    if(nums[i] < nums[j] && nums[k] < nums[j]){
+                        res = Math.min(res,nums[i]+nums[j]+nums[k]);
+                    }
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
 }
