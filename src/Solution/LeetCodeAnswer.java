17c307c3f38f492112f87f93b0213f479321b873
Rek <wujj1997@gmail.com>
2024-05-25 00:26:30 +0800

    leetcode No.2903 pass

--- src/Solution/LeetCodeAnswer.java
+++ src/Solution/LeetCodeAnswer.java
@@ -1912,4 +1912,17 @@ public class LeetCodeAnswer {
         }
         return res;
     }
+    //24.5.25 easy
+    //leetcode No.2903
+    public static int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        int[] res = {-1,-1};
+        for(int i = 0;i < nums.length;i++){
+            for(int j = 0;j < nums.length;j++){
+                if(Math.abs(i-j) >= indexDifference && Math.abs(nums[i]-nums[j]) >= valueDifference){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return res;
+    }
 }
